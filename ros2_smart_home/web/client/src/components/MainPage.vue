842ea5681b4af5af11a0a632bd3bbd995195e6b8
Donggun Ko <dongjuk157@gmail.com>
2021-09-27 17:33:36 +0900

    [FE][Feat][S05P21B301-333] Add main page
    Add main page
    Add 404 page
    Add eslint airbnb
    Add Vue router
    with yarn

--- ros2_smart_home/web/client/src/components/MainPage.vue
+++ ros2_smart_home/web/client/src/components/MainPage.vue
@@ -1,7 +1,20 @@
 <template>
-  <div>
-    Hello, Vue world!
-  </div>
+  <b-container>
+    <b-row>
+      <b-col cols="12" class="btn-main-page" @click="moveAddress">
+        로봇 설정
+      </b-col>
+      <b-col cols="12" class="btn-main-page" @click="moveAddress">
+        지도
+      </b-col>
+      <b-col cols="12" class="btn-main-page" @click="moveAddress">
+        스케줄
+      </b-col>
+      <b-col cols="12" class="btn-main-page" @click="moveAddress">
+        가전 제어
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
@@ -9,10 +22,40 @@ export default {
   name: 'Home',
   components: {
   },
+  methods: {
+    moveAddress(event) {
+      const { innerText } = event.target;
+      switch (innerText) {
+        case '로봇 설정':
+          this.$router.push({ path: '/settings/robot' });
+          break;
+        case '지도':
+          this.$router.push({ path: '/map' });
+          break;
+        case '스케줄':
+          this.$router.push({ path: '/schedule' });
+          break;
+        case '가전 제어':
+          this.$router.push({ path: '/controls' });
+          break;
+        default:
+      }
+    },
+  },
 };
 
 </script>
 
 <style>
-
+  .btn-main-page {
+    padding:0.5rem 1rem 0.5rem 1rem;
+    background-color: grey;
+    background-clip: content-box;
+    color: white;
+    height: 4rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+  }
 </style>
