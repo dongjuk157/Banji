5136fdf2a055006190b884ed018c38cb051d74a0
DongGun Ko <dongjuk157@gmail.com>
2021-09-28 14:31:20 +0900

    [FE][Feat][S05P21B301-334] Create Scheduler with vuetify

--- ros2_smart_home/web/client/src/main.js
+++ ros2_smart_home/web/client/src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import Vuetify from 'vuetify';
 
 import App from './App.vue';
 import router from './router';
@@ -8,8 +9,13 @@ import store from './store';
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+import 'vuetify/dist/vuetify.min.css'; // Ensure you are using css-loader
+
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue);
+
+Vue.use(Vuetify);
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin);
 
