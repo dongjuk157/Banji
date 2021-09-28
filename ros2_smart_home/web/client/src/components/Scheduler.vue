d22e0eb37b73f8061c68326d8a2cef1e72180705
Donggun Ko <dongjuk157@gmail.com>
2021-09-28 22:51:51 +0900

    [FE][Feat][S05P21B301-334] Create adding function
    1. Add Vuetify on Project (because 2)
    2. Add Vuetify Calendar in Scheduler page
    3. Create function about adding schedule on calendar

--- ros2_smart_home/web/client/src/components/Scheduler.vue
+++ ros2_smart_home/web/client/src/components/Scheduler.vue
@@ -1,120 +1,292 @@
 <template>
-  <div>
-    <v-sheet
-      tile
-      height="54"
-      class="d-flex"
+  <div class="pa-5">
+    <v-row>
+      <v-col cols="12" class="mb-4">
+        <v-sheet height="64">
+          <v-toolbar
+            flat
+          >
+            <v-toolbar-title v-if="$refs.calendar">
+              {{ $refs.calendar.title }}
+            </v-toolbar-title>
+            <v-col cols="4">
+              <v-btn
+                fab
+                text
+                small
+                color="grey darken-2"
+                @click="prev"
+              >
+                <v-icon small>
+                  mdi-chevron-left
+                </v-icon>
+              </v-btn>
+            </v-col>
+            <v-col cols="4">
+              <v-btn
+                fab
+                text
+                small
+                color="grey darken-2"
+                @click="setToday"
+              >
+                <v-icon small>
+                  mdi-checkbox-blank-circle-outline
+                </v-icon>
+              </v-btn>
+            </v-col>
+            <v-col cols="4">
+              <v-btn
+                fab
+                text
+                small
+                color="grey darken-2"
+                @click="next"
+              >
+                <v-icon small>
+                  mdi-chevron-right
+                </v-icon>
+              </v-btn>
+            </v-col>
+            <v-spacer></v-spacer>
+          </v-toolbar>
+        </v-sheet>
+        <v-sheet height="400">
+          <v-calendar
+            ref="calendar"
+            v-model="focus"
+            color="primary"
+            :events="events"
+            :event-color="getEventColor"
+            :type="type"
+            @click:more="viewDay"
+            @click:date="viewDay"
+            @change="update"
+          ></v-calendar>
+        </v-sheet>
+      </v-col>
+    </v-row>
+    <v-dialog
+      v-model="dialog"
+      width="500"
     >
-      <v-btn
-        icon
-        class="ma-2"
-        @click="$refs.calendar.prev()"
-      >
-        <v-icon>mdi-chevron-left</v-icon>
-      </v-btn>
-      <v-select
-        v-model="type"
-        :items="types"
-        dense
-        outlined
-        hide-details
-        class="ma-2"
-        label="type"
-      ></v-select>
-      <v-select
-        v-model="mode"
-        :items="modes"
-        dense
-        outlined
-        hide-details
-        label="event-overlap-mode"
-        class="ma-2"
-      ></v-select>
-      <v-select
-        v-model="weekday"
-        :items="weekdays"
-        dense
-        outlined
-        hide-details
-        label="weekdays"
-        class="ma-2"
-      ></v-select>
-      <v-spacer></v-spacer>
-      <v-btn
-        icon
-        class="ma-2"
-        @click="$refs.calendar.next()"
-      >
-        <v-icon>mdi-chevron-right</v-icon>
-      </v-btn>
-    </v-sheet>
-    <v-sheet height="600">
-      <v-calendar
-        ref="calendar"
-        v-model="value"
-        :weekdays="weekday"
-        :type="type"
-        :events="events"
-        :event-overlap-mode="mode"
-        :event-overlap-threshold="30"
-        :event-color="getEventColor"
-        @change="getEvents"
-      ></v-calendar>
-    </v-sheet>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          fab
+          text
+          small
+          color="grey darken-2"
+          v-bind="attrs"
+          v-on="on"
+        >
+          <v-icon small>
+            mdi-plus
+          </v-icon>
+        </v-btn>
+      </template>
+
+      <v-card>
+        <v-card-title class="text-h5 grey lighten-2">
+          일정 추가
+        </v-card-title>
+        <v-card-text>
+          <v-row>
+            <v-col cols="12">
+              <v-text-field
+                v-model="schedule.name"
+                label="내용"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12" md="6">
+              <v-text-field
+                v-model="schedule.start.date"
+                label="시작일"
+                type="date"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12" md="6">
+              <v-text-field
+                v-model="schedule.start.time"
+                label="시작시간"
+                type="time"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12" md="6">
+              <v-text-field
+                v-model="schedule.end.date"
+                label="종료일"
+                type="date"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12" md="6">
+              <v-text-field
+                v-model="schedule.end.time"
+                label="종료시간"
+                type="time"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="primary"
+            text
+            @click="addSchedule"
+          >
+            ADD
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
   </div>
 </template>
 
 <script>
 export default {
   data: () => ({
+    schedule: {
+      name: '',
+      start: {
+        date: '',
+        time: '',
+      },
+      end: {
+        data: '',
+        time: '',
+      },
+    },
+    dialog: false,
+    focus: '',
     type: 'month',
-    types: ['month', 'week', 'day', '4day'],
-    mode: 'stack',
-    modes: ['stack', 'column'],
-    weekday: [0, 1, 2, 3, 4, 5, 6],
-    weekdays: [
-      { text: 'Sun - Sat', value: [0, 1, 2, 3, 4, 5, 6] },
-      { text: 'Mon - Sun', value: [1, 2, 3, 4, 5, 6, 0] },
-      { text: 'Mon - Fri', value: [1, 2, 3, 4, 5] },
-      { text: 'Mon, Wed, Fri', value: [1, 3, 5] },
-    ],
-    value: '',
+    typeToLabel: {
+      month: 'Month',
+      day: 'Day',
+    },
+    selectedEvent: {},
+    selectedElement: null,
+    selectedOpen: false,
     events: [],
     colors: ['blue', 'indigo', 'deep-purple', 'cyan', 'green', 'orange', 'grey darken-1'],
-    names: ['Meeting', 'Holiday', 'PTO', 'Travel', 'Event', 'Birthday', 'Conference', 'Party'],
+    // names: ['Meeting', 'Holiday', 'PTO', 'Travel', 'Event', 'Birthday', 'Conference', 'Party'],
   }),
+  mounted() {
+    this.$refs.calendar.checkChange();
+    this.loadLocalstorage();
+  },
   methods: {
-    getEvents({ start, end }) {
-      const events = [];
-
-      const min = new Date(`${start.date}T00:00:00`);
-      const max = new Date(`${end.date}T23:59:59`);
-      const days = (max.getTime() - min.getTime()) / 86400000;
-      const eventCount = this.rnd(days, days + 20);
-
-      for (let i = 0; i < eventCount; i += 1) {
-        const allDay = this.rnd(0, 3) === 0;
-        const firstTimestamp = this.rnd(min.getTime(), max.getTime());
-        const first = new Date(firstTimestamp - (firstTimestamp % 900000));
-        const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000;
-        const second = new Date(first.getTime() + secondTimestamp);
-
-        events.push({
-          name: this.names[this.rnd(0, this.names.length - 1)],
-          start: first,
-          end: second,
-          color: this.colors[this.rnd(0, this.colors.length - 1)],
-          timed: !allDay,
-        });
-      }
-
-      this.events = events;
+    viewDay({ date }) {
+      this.focus = date;
+      this.type = 'day';
     },
     getEventColor(event) {
       return event.color;
     },
+    setToday() {
+      this.focus = '';
+      this.type = 'month';
+    },
+    prev() {
+      this.$refs.calendar.prev();
+    },
+    next() {
+      this.$refs.calendar.next();
+    },
+    showEvent({ nativeEvent, event }) {
+      const open = () => {
+        this.selectedEvent = event;
+        this.selectedElement = nativeEvent.target;
+        requestAnimationFrame(() => requestAnimationFrame(() => {
+          this.selectedOpen = true;
+        }));
+      };
+
+      if (this.selectedOpen) {
+        this.selectedOpen = false;
+        requestAnimationFrame(() => requestAnimationFrame(() => open()));
+      } else {
+        open();
+      }
+
+      nativeEvent.stopPropagation();
+    },
+    update() {
+      const events = [];
+      // const min = new Date(`${start.date}T00:00:00`);
+      // const max = new Date(`${end.date}T23:59:59`);
+      // const days = (max.getTime() - min.getTime()) / 86400000;
+      // const eventCount = this.rnd(days, days + 20);
+      // for (let i = 0; i < eventCount; i += 1) {
+      //   const allDay = this.rnd(0, 3) === 0;
+      //   const firstTimestamp = this.rnd(min.getTime(), max.getTime());
+      //   const first = new Date(firstTimestamp - (firstTimestamp % 900000));
+      //   const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000;
+      //   const second = new Date(first.getTime() + secondTimestamp);
+
+      //   events.push({
+      //     name: this.names[this.rnd(0, this.names.length - 1)],
+      //     start: first,
+      //     end: second,
+      //     color: this.colors[this.rnd(0, this.colors.length - 1)],
+      //     timed: !allDay,
+      //   });
+      // }
+
+      this.events = events;
+      this.loadLocalstorage();
+    },
     rnd(a, b) {
       return Math.floor((b - a + 1) * Math.random()) + a;
+    },
+    addSchedule() {
+      const startString = this.schedule.start.time ? `${this.schedule.start.date} ${this.schedule.start.time}` : this.schedule.start.date;
+      const endString = this.schedule.end.time ? `${this.schedule.end.date} ${this.schedule.end.time}` : this.schedule.end.date;
+      const item = {
+        name: this.schedule.name,
+        start: startString,
+        end: endString,
+        color: this.colors[this.rnd(0, this.colors.length - 1)],
+        timed: this.schedule.start.time && this.schedule.end.time,
+      };
+      // 0. check all inputs right
+      if (!this.schedule.name
+        || !this.schedule.start.date
+        || !this.schedule.end.date) {
+        return;
+      }
+      // 1. add
+      const events = [...this.events];
+      events.push(item);
+      this.events = events;
+      // 1-1. save localstorage
+      this.saveLocalstorage();
+      // 2. initialize
+      const initialSchedule = {
+        name: '',
+        start: {
+          date: '',
+          time: '',
+        },
+        end: {
+          data: '',
+          time: '',
+        },
+      };
+      this.schedule = initialSchedule;
+      // 3. dialog close
+      this.dialog = false;
+    },
+    saveLocalstorage() {
+      localStorage.setItem('events', JSON.stringify(this.events));
+    },
+    loadLocalstorage() {
+      const eventsString = localStorage.getItem('events');
+      const events = JSON.parse(eventsString);
+      if (events) {
+        this.events = events;
+      }
     },
   },
 };
--- ros2_smart_home/web/client/src/main.js
+++ ros2_smart_home/web/client/src/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
-import Vuetify from 'vuetify';
 
 import App from './App.vue';
 import router from './router';
@@ -9,13 +8,13 @@ import store from './store';
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
-
-import 'vuetify/dist/vuetify.min.css'; // Ensure you are using css-loader
+import vuetify from './plugins/vuetify';
+import 'roboto-fontface/css/roboto/roboto-fontface.css';
+import '@mdi/font/css/materialdesignicons.css';
 
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue);
 
-Vue.use(Vuetify);
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin);
 
@@ -24,5 +23,6 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
+  vuetify,
   render: (h) => h(App),
 }).$mount('#app');
