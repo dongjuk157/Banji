5f75267f92ad0ecce279297429331595a9a08411
DongGun Ko <dongju0628@naver.com>
2021-10-01 10:18:37 +0900

    [FE][Fix] Modify Socket.io version
    1. Modify Socket version 4.2 into 2.3
    2. Modify porting number in dockerfile
    3. Modify url web server (test)

--- ros2_smart_home/web/server/server.js
+++ ros2_smart_home/web/server/server.js
@@ -16,9 +16,10 @@ const picPath = path.join(__dirname, "/../client");
 
 // 로직 1. WebSocket 서버, WebClient 통신 규약 정의
 const server = require('http').createServer(app);
-const io = require('socket.io')(server, { cors: { origin: "*" } })
-
-
+const io = require('socket.io')(server, { 
+    cors: { origin: "*" },
+    transports: ['websocket', 'polling'],
+})
 
 var fs = require('fs'); // required for file serving
 
@@ -87,116 +88,77 @@ io.on('connection', socket => {
     })
 
     // 로직 4. WebClient에서 메시지 수신시 사용자에게 메시지 전달
-    //data = ""
     socket.on('sendStateRefreshToServer', (data) => {
         console.log('sendStateRefresh')
         socket.to(roomName).emit('sendStateRefresh', data);
     });
 
-    // 에어컨 작동
     socket.on('sendAirPurifierOnToServer', (data) => {
         console.log('가전제품 ON/OFF')
         socket.to(roomName).emit('sendAirConOn', data);
     });
 
-    //data = ""
     socket.on('sendScanOnToServer', (data) => {
         console.log('sendScanOn')
         socket.to(roomName).emit('sendScanOn', data);
     });
-    //data = ""
+
     socket.on('sendScanOffToServer', (data) => {
         console.log('sendScanOff')
         socket.to(roomName).emit('sendScanOff', data);
     });
     
-    //data = ""
     socket.on('sendGetRegObjToServer', (data) => {
         console.log('sendGetRegObj')
         socket.to(roomName).emit('sendGetRegObj', data);
     });
 
-    //data = "name"
     socket.on('sendObjOnToServer', (data) => {
         console.log('sendObjOn')
         socket.to(roomName).emit('sendObjOn', data);
     });
-    //data = "name"
+
     socket.on('sendObjOffToServer', (data) => {
         console.log('sendObjOff')
         socket.to(roomName).emit('sendObjOff', data);
     });
 
-    //data = [uid, name, net_status, dev_status, [imgX, imgY]]
+
     socket.on('sendRegistObjToServer', (data) => {
         console.log('sendRegistObj')
         socket.to(roomName).emit('sendRegistObj', data);
     });
-    //data = "name"
+
     socket.on('sendRemoveObjToServer', (data) => {
         console.log('sendRemoveObj')
         socket.to(roomName).emit('sendRemoveObj', data);
     });
 
-    //data = ""
     socket.on('sendGetRocationToServer', (data) => {
         console.log('sendGetRocation')
         socket.to(roomName).emit('sendGetRocation', data);
     })
 
-    //data = ""
     socket.on('sendSecurityModeOnToServer', (data) => {
         console.log('sendSecurityModeOn')
         socket.to(roomName).emit('sendSecurityModeOn', data);
     })
 
-    //data = ""
     socket.on('sendSecurityModeOffToServer', (data) => {
         console.log('sendSecurityModeOff')
         socket.to(roomName).emit('sendSecurityModeOff', data);
     })
 
-    //data = ""
     socket.on('sendGetSecurityStatusToServer', (data) => {
         console.log('sendGetSecurityStatus')
         socket.to(roomName).emit('sendGetSecurityStatus', data);
     })
     
-    //data = ""
     socket.on('sendGetThiefToServer', (data) => {
         console.log('sendGetThief')
         socket.to(roomName).emit('sendGetThief', data);
     })
 
-
-    // socket.on('safety_status', (message) => {
-    //     socket.to(roomName).emit('sendSafetyStatus', message);
-    // });
-
-    // socket.on('PatrolStatus', (message) => {
-    //     socket.to(roomName).emit('sendPatrolStatus', message);
-    // });
-
-    // socket.on('PatrolOnToServer', (data) => {
-    //     socket.to(roomName).emit('patrolOn', data);
-    //     console.log('Patrol On!');
-    // });
-
-    // socket.on('PatrolOffToServer', (data) => {
-    //     socket.to(roomName).emit('patrolOff', data);
-    // });
-
-    // socket.on('turnleftToServer', (data) => {
-    //     socket.to(roomName).emit('turnleft', data);
-    // });
-
-    // socket.on('gostraightToServer', (data) => {
-    //     socket.to(roomName).emit('gostraight', data);
-    // });
-
-    // socket.on('turnrightToServer', (data) => {
-    //     socket.to(roomName).emit('turnright', data);
-    // });
 
     socket.on('disconnect', () => {
         console.log('disconnected from server');
