f92d7744fb2b5f6d625ba15cf56a312abcf3688a
DongGun Ko <dongju0628@naver.com>
2021-10-03 17:53:29 +0900

    [FE][Feat] Seperate a component, camera view
    1. helloworld.vue: Add the component (test page)
    2. CameraView.vue: Create camera view component

--- ros2_smart_home/web/client/src/components/HelloWorld.vue
+++ ros2_smart_home/web/client/src/components/HelloWorld.vue
@@ -1,73 +1,30 @@
 <template>
   <v-row class="pa-3">
-    <!-- 로직 2. 침입자 인지 시점 이미지 뷰어 생성 -->
-    <!-- <v-col cols="6">
-      <v-img :src="imageSrc" width="480" @load="imageLoad" @error="imageError"></v-img>
-    </v-col> -->
-    <!-- 로직 4. 수동 컨트롤 버튼 생성 -->
-    <!-- <v-col cols="6">
+    <!-- 수동 컨트롤 버튼 생성 -->
+    <v-col cols="6">
       <p>Manual Controller</p>
       <Move />
-    </v-col> -->
-    <v-col>
-      <v-btn
-        @click="getCam"
-      >
-        cam
-      </v-btn>
-      <v-img
-        :src="imgSrc"
-      />
     </v-col>
+    <!-- 카메라 이미지 뷰어 생성 -->
+    <CameraView />
   </v-row>
 </template>
 
 <script>
-// import Move from './Move.vue';
+import CameraView from './CameraView.vue';
+import Move from './Move.vue';
 
 export default {
   components: {
-    // Move,
+    CameraView,
+    Move,
   },
   data() {
     return {
-      time: new Date().getTime(),
-      imgSrc: null,
     };
   },
-  mounted() {
-    this.$socket.on('front_robotview_back', (msg) => {
-      console.log(msg);
-      const src = `data:image/png;base64,${msg}`;
-      this.imgSrc = src;
-    });
-  },
-  methods: {
-    getCam() {
-      console.log('cam');
-      this.$socket.emit('back_robotview_front', 'cam');
-    },
-    imageLoad() {
-      setTimeout(() => {
-        this.time = new Date().getTime();
-        // console.log(this.time)
-      }, 50);
-    },
-    imageError() {
-      setTimeout(() => {
-        this.time = new Date().getTime();
-        // console.log(this.time)
-      }, 50);
-    },
-  },
-  computed: {
-    imageSrc() {
-      const fileName = 'cam.jpg';
-      const query = '?t=';
-      // console.log(fileName + query + this.time)
-      return fileName + query + this.time;
-    },
-  },
+  methods: {},
+  computed: {},
 };
 </script>
 
