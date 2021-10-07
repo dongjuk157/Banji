00a7a7b47422aa331efe748467eea250938555c8
Donggun Ko <dongjuk157@gmail.com>
2021-10-07 18:27:17 +0900

    [FE][Fix] 로그아웃 오타 수정

--- ros2_smart_home/web/client/src/views/About.vue
+++ ros2_smart_home/web/client/src/views/About.vue
@@ -3,20 +3,18 @@
     <v-img src="../assets/mantoman.jpg"></v-img>
     <h6 style="margin: 10px auto;">mady by mantoman</h6>
     <p class="made-team">고동건 양준서 이광교 이진영 정대진 천주용</p>
-    <v-btn @click="logout">
+    <v-btn @click="Logout">
       Logout
     </v-btn>
   </div>
 </template>
 <script>
-import HomeVue from './Home.vue';
-
 export default {
   methods: {
 
     Logout() {
       localStorage.removeItem('isLogin');
-      this.$router.push(HomeVue);
+      this.$router.push({ path: '/' });
       window.location.reload();
     },
   },
